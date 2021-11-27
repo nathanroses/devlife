0d4a0df57bd50093fa3cc1dd73ef10c1e75f3034
Nathan Rose <87315662+nathanroses@users.noreply.github.com>
2021-11-27 13:03:36 -0600

    updated animation added text

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,8 +5,9 @@ const Intro = () => {
         <div className="i">
             <div className="i-left">
               <div className="i-left-wrapper">
-                  <h2 className="i-intro"> Hello, My name is </h2>
+                  <h2 className="i-intro"> Hello, My Name is </h2>
                   <h1 className="i-name">Nathan Rose</h1>
+                 <div className="i-title">
                   <div className="i-title-wrapper">
                       <div className="i-title-item">Web Developer</div>
                       <div className="i-title-item">React Native</div>
@@ -14,6 +15,7 @@ const Intro = () => {
                       <div className="i-title-item">Python</div>
                       <div className="i-title-item">Designer</div>
                   </div>
+                </div> 
               </div>
             </div>
             <div className="i-right">right</div>
